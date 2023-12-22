3bfa055c06fe9a27da7a77d80226992b3402a0a5
jatinbphp <jatin.b.php@gmail.com>
2023-12-22 17:33:09 +0530

    creating logout functionality

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return redirect()->route('login');
 });
 
-Auth::routes();
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); 
+});
+
+
+Route::get('/home', function () {
+    return "true";
+});
+
+
+
+
+
+
+
+
+// Auth::routes();
+
